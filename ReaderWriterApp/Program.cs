296419499121250cbfab0d5e56eaa301eadb028b
Ivan Suchkov <vekxes@yandex.ru>
2023-09-20 00:06:56 +0500

    Add project files.

---
+++ ReaderWriterApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReaderWriterApp;
+
+Parallel.For(0, 50, i =>
+{
+    if (i % 2 == 0)
+        Server.AddToCount(2);
+    else
+        Console.WriteLine(Server.GetCount());
+});
+Console.WriteLine(Server.GetCount());
+Console.WriteLine("Done.");
+Console.ReadKey();
---
+++ ReaderWriterApp/Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderWriterApp
+{
+    public static class Server
+    {
+        private static int count;
+        private static ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            count += value;
+            locker.ExitWriteLock();
+        }
+
+        public static int GetCount()
+        {
+            locker.EnterWriteLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+    }
+}
