13126cdb1ef09eadad66dda584f5cace362b8dfe
Ivan Suchkov <vekxes@yandex.ru>
2023-09-20 09:45:29 +0500

    1

--- ReaderWriterApp/Program.cs
+++ ReaderWriterApp/Program.cs
@@ -1,13 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ReaderWriterApp;
 
-Parallel.For(0, 50, i =>
+Parallel.For(0, 100, i =>
 {
     if (i % 2 == 0)
         Server.AddToCount(2);
     else
         Console.WriteLine(Server.GetCount());
 });
+Console.WriteLine("Stop");
+
 Console.WriteLine(Server.GetCount());
-Console.WriteLine("Done.");
+
 Console.ReadKey();
